94cdd3e6c1ee8687f55bd7f1a4d53fb8d61bc27b
Le127 <nereocarbonara@gmail.com>
2021-08-31 10:42:27 -0300

    diseño listo

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:estado_singleton_app/pages/pagina2_page.dart';
+import 'package:flutter/material.dart';
+import 'package:estado_singleton_app/pages/pagina1_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: "pagina1",
+        routes: {
+          "pagina1": (_) => Pagina1Page(),
+          "pagina2": (_) => Pagina2Page(),
+        });
+  }
+}
