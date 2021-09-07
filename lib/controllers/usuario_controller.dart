165dc858438ab87d1a629f0e3cab7779a1efd1a4
Le127 <nereocarbonara@gmail.com>
2021-09-06 22:25:13 -0300

    video 169

---
+++ lib/controllers/usuario_controller.dart
@@ -0,0 +1,6 @@
+import 'package:estado_singleton_app/models/usuario.dart';
+
+class UsuarioController {
+  bool existeUsuario = false;
+  late Usuario usuario;
+}
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
+//import 'package:get/get.dart';
 
 class Pagina2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //print(Get.arguments["nombre"]);
+
     return Scaffold(
       appBar: AppBar(title: Text("Pagina2")),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MaterialButton(
-              child: Text("Establecer Usuario",
-                  style: TextStyle(color: Colors.white)),
-              color: Colors.blue,
-              onPressed: () {}),
-          SizedBox(height: 10),
-          MaterialButton(
-              child:
-                  Text("Cambiar Edad", style: TextStyle(color: Colors.white)),
-              color: Colors.blue,
-              onPressed: () {}),
-          SizedBox(height: 10),
-          MaterialButton(
-              child: Text("Añadir profesion",
-                  style: TextStyle(color: Colors.white)),
-              color: Colors.blue,
-              onPressed: () {}),
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+                child: Text("Establecer Usuario",
+                    style: TextStyle(color: Colors.white)),
+                color: Colors.blue,
+                onPressed: () {}),
+            SizedBox(height: 10),
+            MaterialButton(
+                child:
+                    Text("Cambiar Edad", style: TextStyle(color: Colors.white)),
+                color: Colors.blue,
+                onPressed: () {}),
+            SizedBox(height: 10),
+            MaterialButton(
+                child: Text("Añadir profesion",
+                    style: TextStyle(color: Colors.white)),
+                color: Colors.blue,
+                onPressed: () {}),
+          ],
+        ),
+      ),
     );
   }
 }
